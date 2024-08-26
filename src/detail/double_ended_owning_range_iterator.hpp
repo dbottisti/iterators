161f21f66e49aeec69153c5ce1b327248b2f08d2
Dave Bottisti <dbottisti@gmail.com>
2024-08-26 19:30:01 +0200

    Refactor Owning iterators to use non-owning iterators

--- src/detail/double_ended_owning_range_iterator.hpp
+++ src/detail/double_ended_owning_range_iterator.hpp
@@ -2,51 +2,25 @@
 #define ITERATORS_DETAIL_DOUBLE_ENDED_OWNING_RANGE_ITERATOR_HPP
 
 #include "base.hpp"
-#include "double_ended.hpp"
+#include "detail/double_ended_range_iterator.hpp"
 
 namespace iter {
 namespace detail {
 
 template <typename Collection>
 class DoubleEndedOwningRangeIterator
-    : public Iterator<DoubleEndedOwningRangeIterator<Collection>,
-                      std::remove_reference_t<decltype(*std::begin(
-                          std::declval<Collection>()))>>,
-      public DoubleEndedIterator<DoubleEndedOwningRangeIterator<Collection>,
-                                 std::remove_reference_t<decltype(*std::begin(
-                                     std::declval<Collection>()))>> {
-    using IteratorType = decltype(std::begin(std::declval<Collection>()));
-
-public:
-    using value_type = std::remove_reference_t<decltype(*std::begin(
+    : public DoubleEndedRangeIterator<decltype(std::begin(
+          std::declval<Collection>()))> {
+    using Super = DoubleEndedRangeIterator<decltype(std::begin(
         std::declval<Collection>()))>;
 
+public:
     DoubleEndedOwningRangeIterator(Collection collection)
         : collection_{std::move(collection)},
-          current_{std::begin(collection_)},
-          end_{std::end(collection_)} {}
-
-    std::optional<value_type> next() {
-        if (current_ == end_) {
-            return std::nullopt;
-        }
-
-        return *(current_++);
-    }
-
-    std::optional<value_type> next_back() {
-        if (current_ == end_) {
-            return std::nullopt;
-        }
-
-        end_--;
-        return *end_;
-    }
+          Super{std::begin(collection_), std::end(collection_)} {}
 
 private:
     Collection collection_;
-    IteratorType current_;
-    IteratorType end_;
 };
 
 }  // namespace detail
--- src/detail/owning_range_iterator.hpp
+++ src/detail/owning_range_iterator.hpp
@@ -2,38 +2,24 @@
 #define ITERATORS_DETAIL_OWNING_RANGE_ITERATOR_HPP
 
 #include "base.hpp"
+#include "detail/range_iterator.hpp"
 
 namespace iter {
 namespace detail {
 
 template <typename Collection>
 class OwningRangeIterator
-    : public Iterator<OwningRangeIterator<Collection>,
-                      std::remove_reference_t<decltype(*std::begin(
-                          std::declval<Collection>()))>> {
-    using IteratorType = decltype(std::begin(std::declval<Collection>()));
+    : public RangeIterator<decltype(std::begin(std::declval<Collection>()))> {
+    using Super
+        = RangeIterator<decltype(std::begin(std::declval<Collection>()))>;
 
 public:
-    using value_type
-        = std::remove_reference_t<decltype(*std::declval<IteratorType>())>;
-
     OwningRangeIterator(Collection collection)
         : collection_{std::move(collection)},
-          current_{std::begin(collection_)},
-          end_{std::end(collection_)} {}
-
-    std::optional<value_type> next() {
-        if (current_ == end_) {
-            return std::nullopt;
-        }
-
-        return *(current_++);
-    }
+          Super{std::begin(collection_), std::end(collection_)} {}
 
 private:
     Collection collection_;
-    IteratorType current_;
-    IteratorType end_;
 };
 
 }  // namespace detail
